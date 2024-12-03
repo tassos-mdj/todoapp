5ef7ac78d5b8205e5334c857bc5f729fb40b3ac4
tassos-mdj <164147506+tassos-mdj@users.noreply.github.com>
2024-12-03 20:37:39 +0200

    Refactor screenController for dry, add task filtering/sorting logic

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import "./style.css";
 import { Storage } from "./storageController.js";
 import { user } from "./user.js";
-import { Entry } from "./entry.js";
-import { createDashboard, welcomeScreen } from "./screenController.js";
+import { createDashboard, welcomeScreen, displayContent } from "./screenController.js";
+import { format } from "date-fns"; 
+
+
+const currentDate = format(new Date(), "yyyy-MM-dd");
 
 const index = [
     {
@@ -12,12 +15,14 @@ const index = [
         {
             title: 'Groceries',
             description: 'Flour, Soap, Milk',
-            categories: ['home', 'chores']
+            categories: ['home', 'chores'],
+            date: '2024-12-03'
         },
         {
             title: 'Bar supplies',
             description: 'vodka, whiskey, gin',
-            categories: ['home', 'fun']
+            categories: ['home', 'fun'],
+            date: '2024-12-15'
         }
     ]
     },
@@ -28,12 +33,14 @@ const index = [
             {
                 title: 'Vacation Planning',
                 description: 'Get tickets, renew passport',
-                categories: ['vacation', 'family', 'home']
+                categories: ['vacation', 'family', 'home'],
+                date: '2024-12-06'
             },
             {
                 title: 'Gym routine',
                 description: 'Stand-ups, sit-ups, pull-ups, push-ups',
-                categories: ['health', 'fun']
+                categories: ['health', 'fun'],
+                date: '2024-12-08'
             }
         ]
     }
@@ -70,5 +77,36 @@ function loadDashboard(activeUser) {
         createDashboard(userData);
     }
 
+    const aside = document.querySelector('menu');
+    const lis = aside.querySelectorAll('li');
+
+    for (let i = 0; i < lis.length; i++) {
+        lis[i].addEventListener('click', function () {
+            switch (lis[i].id) {
+                case 'agenda':
+                    displayContent('agenda', loadAgenda(userData.notes));
+                    break;
+                case 'today':
+                    displayContent('today', loadToday(userData.notes));
+                    break;
+                case 'view-all':
+                    //loadViewAll(userData);
+                    console.log('Clicked View All!');
+                    break;
+                case 'cat-all':
+                    displayContent('agenda', userData);
+                    break;
+                
+            }
+                });
+    }
   
+}
+
+function loadAgenda(notes) {
+    return notes.sort((a,b) => new Date(a.date) - new Date(b.date));
+}
+
+function loadToday(notes) {
+    return notes.filter((note) => note.date === currentDate);
 }
--- src/screenController.js
+++ src/screenController.js
@@ -1,7 +1,7 @@
-import { body } from "./index.js";
 import { login as loginFunction } from "./index.js"
-import { currentIndex } from "./index.js";
-import { user } from "./user.js";
+import { format } from "date-fns";
+
+
 
 export function welcomeScreen() {
     const login = document.querySelector('#login-button');
@@ -15,49 +15,161 @@ export function createDashboard(userData){
     const userNameDisplay = document.querySelector('.user-name-display');
     userNameDisplay.textContent = userData.username;
 
-    loadAgenda(userData);
-    
+    displayContent('agenda', userData.notes);
 }
 
-function loadAgenda(userData) {
-    const userNotes = userData.notes;
-    console.log(userNotes);
+export function displayContent(section, userNotes) {
+    resetActiveMenuItems();
+
+    const currentSection = document.getElementById(section);
+    currentSection.classList.add('active-menu-item');
+
+    
     const dataArea = document.querySelector('.data-area');
-    console.log(dataArea);
+    dataArea.innerHTML = '';
 
     const heading = document.createElement('h2');
-    heading.textContent = 'Agenda';
-    console.log(heading);
+    heading.textContent = section;
     dataArea.appendChild(heading);
     
     const article = document.createElement('article');
     article.classList.add('list-view');
     
+    if (userNotes.length === 0) {
+        let task = document.createElement('div');
+        task.classList.add('task');
+
+        let taskTitle = document.createElement('h3');
+        taskTitle.classList.add('task-title');
+        taskTitle.textContent = 'All clear, nothing on your plate!';
+        task.appendChild(taskTitle);
+        article.appendChild(task);
+    }
 
     for (let note of userNotes) {
     
-    let task = document.createElement('div');
-    task.classList.add('task');
+        let task = document.createElement('div');
+        task.classList.add('task');
 
-    let taskTitle = document.createElement('h3');
-    taskTitle.classList.add('task-title');
-    taskTitle.textContent = note.title;
-    task.appendChild(taskTitle);
+        let taskTitle = document.createElement('h3');
+        taskTitle.classList.add('task-title');
+        taskTitle.textContent = note.title;
+        task.appendChild(taskTitle);
 
-    let taskDescription = document.createElement('p');
-    taskDescription.classList.add('task-description');
-    taskDescription.textContent = note.description;
-    task.appendChild(taskDescription);
+        let taskDescription = document.createElement('p');
+        taskDescription.classList.add('task-description');
+        taskDescription.textContent = note.description;
+        task.appendChild(taskDescription);
 
-    let dueDate = document.createElement('div');
-    dueDate.classList.add('due-date');
+        let dueDate = document.createElement('div');
+        dueDate.classList.add('due-date');
+        dueDate.textContent = note.date;
+        task.appendChild(dueDate);
     
-    article.appendChild(task);
-
+        article.appendChild(task);
     }
+
+
 
     dataArea.appendChild(article);
 }
 
+function resetActiveMenuItems() {
+    document.getElementById('agenda').classList.remove('active-menu-item');
+    document.getElementById('today').classList.remove('active-menu-item');
+    document.getElementById('view-all').classList.remove('active-menu-item');
+}
 
+
+// export function loadAgenda(userData) {
+    //     resetActiveMenuItems();
+    
+    //     const menuAgenda = document.getElementById('agenda');
+    //     menuAgenda.classList.add('active-menu-item');
+    
+    //     const userNotes = userData.notes;
+    //     const dataArea = document.querySelector('.data-area');
+    //     dataArea.innerHTML = '';
+    
+    //     const heading = document.createElement('h2');
+    //     heading.textContent = 'Agenda';
+    //     dataArea.appendChild(heading);
+        
+    //     const article = document.createElement('article');
+    //     article.classList.add('list-view');
+        
+    
+    //     for (let note of userNotes) {
+        
+    //         let task = document.createElement('div');
+    //         task.classList.add('task');
+    
+    //         let taskTitle = document.createElement('h3');
+    //         taskTitle.classList.add('task-title');
+    //         taskTitle.textContent = note.title;
+    //         task.appendChild(taskTitle);
+    
+    //         let taskDescription = document.createElement('p');
+    //         taskDescription.classList.add('task-description');
+    //         taskDescription.textContent = note.description;
+    //         task.appendChild(taskDescription);
+    
+    //         let dueDate = document.createElement('div');
+    //         dueDate.classList.add('due-date');
+    //         dueDate.textContent = note.date;
+    //         task.appendChild(dueDate);
+        
+    //         article.appendChild(task);
+    //     }
+    
+    //     dataArea.appendChild(article);
+    // }
+    
+    // export function loadToday(userData) {
+    //     resetActiveMenuItems();
+        
+    //     const menuToday = document.getElementById('today');
+    //     menuToday.classList.add('active-menu-item');
+    
+    //     const userNotes = userData.notes;
+    //     const dataArea = document.querySelector('.data-area');
+    //     dataArea.innerHTML = '';
+    
+    //     const heading = document.createElement('h2');
+    //     heading.textContent = 'Today';
+    //     dataArea.appendChild(heading);
+        
+    //     const article = document.createElement('article');
+    //     article.classList.add('list-view');
+        
+    
+    //     for (let note of userNotes) {
+    
+    //         if (note.date === currentDate) {
+        
+    //             let task = document.createElement('div');
+    //             task.classList.add('task');
+    
+    //             let taskTitle = document.createElement('h3');
+    //             taskTitle.classList.add('task-title');
+    //             taskTitle.textContent = note.title;
+    //             task.appendChild(taskTitle);
+    
+    //             let taskDescription = document.createElement('p');
+    //             taskDescription.classList.add('task-description');
+    //             taskDescription.textContent = note.description;
+    //             task.appendChild(taskDescription);
+    
+    //             let dueDate = document.createElement('div');
+    //             dueDate.classList.add('due-date');
+    //             dueDate.textContent = note.date;
+    //             task.appendChild(dueDate);
+        
+    //             article.appendChild(task);
+    //         }
+    //     }
+    
+    //     dataArea.appendChild(article);
+    
+    // }
 
